6488eece6160be7abd41a2eeb7e932acc9cf887f
berprado <112181004+berprado@users.noreply.github.com>
2025-07-03 22:05:46 -0400

    Update servicios.py
    documentando codigo

--- servicios.py
+++ servicios.py
@@ -1,5 +1,11 @@
 from zeep import Client
 
+# URL del WSDL del servicio de la administración tributaria boliviana (SIAT).
+# Este WSDL define los métodos disponibles para la interoperabilidad con el sistema de facturación electrónica.
 wsdl_url = 'https://pilotosiatservicios.impuestos.gob.bo/v2/FacturacionCodigos?wsdl'
+
+# Inicializa el cliente Zeep para consumir servicios SOAP definidos en el WSDL.
 client = Client(wsdl_url)
+
+# Imprime la lista de servicios disponibles, útil para explorar y conocer los métodos expuestos por el SIAT.
 print(client.service)  # Debería mostrar los servicios disponibles
